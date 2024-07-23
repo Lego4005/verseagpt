b92f91b28e3b36ca7b394da7f9c6669471dcac05
Director of Fun <87780873+Lego4005@users.noreply.github.com>
2024-07-23 04:27:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import os
+import nltk
+
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
+required_packages = ['punkt', 'averaged_perceptron_tagger']
+
+for package in required_packages:
+    try:
+        nltk.data.find(f'tokenizers/{package}')
+    except LookupError:
+        nltk.download(package, download_dir=nltk_data_path)
+
+# Your application code here
+
+
 from backend.server import app
 from dotenv import load_dotenv
 load_dotenv()
